f26557346c26e9b90a7054721d3034013f2121f8
Act-mall <zainnur.akmalz@gmail.com>
2025-11-08 17:29:38 +0800

    Menambahkan kode CRUD Kontak

--- src/controller/KontakController.java
+++ src/controller/KontakController.java
@@ -1,38 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
 import model.Kontak;
 import model.KontakDAO;
 import java.sql.SQLException;
 import java.util.List;
-
-private KontakDAO contactDAO;
+/**
+ *
+ * @author Acer
+ */
+public class KontakController {
+    private KontakDAO contactDAO;
 
 public KontakController() {
     contactDAO = new KontakDAO();
 }
+
 // Method mengambil semua data kontak
 public List<Kontak> getAllContacts() throws SQLException {
     return contactDAO.getAllContacts();
 }
+
 // Method menambah kontak
-public void addContact(String nama, String nomorTelepon, String kategori)
-throws SQLException {
+public void addContact(String nama, String nomorTelepon, String kategori) throws SQLException {
     Kontak contact = new Kontak(0, nama, nomorTelepon, kategori);
     contactDAO.addContact(contact);
 }
+
 // Method mengupdate kontak
-public void updateContact(int id, String nama, String nomorTelepon,
-String kategori) throws SQLException {
+public void updateContact(int id, String nama, String nomorTelepon, String kategori) throws SQLException {
     Kontak contact = new Kontak(id, nama, nomorTelepon, kategori);
     contactDAO.updateContact(contact);
 }
+
 // Method menghapus kontak
 public void deleteContact(int id) throws SQLException {
     contactDAO.deleteContact(id);
 }
+
 // Method pencarian kontak
 public List<Kontak> searchContacts(String keyword) throws SQLException {
     return contactDAO.searchContacts(keyword);
 }
-public boolean isDuplicatePhoneNumber(String nomorTelepon, Integer
-excludeId) throws SQLException {
+
+// Method untuk mengecek duplikasi nomor telepon
+public boolean isDuplicatePhoneNumber(String nomorTelepon, Integer excludeId) throws SQLException {
     return contactDAO.isDuplicatePhoneNumber(nomorTelepon, excludeId);
+}
 }
--- src/model/Kontak.java
+++ src/model/Kontak.java
@@ -1,36 +1,41 @@
-private int id;
+
+package model;
+
+public class Kontak {
+    private int id;
 private String nama;
 private String nomorTelepon;
 private String kategori;
 public Kontak(int id, String nama, String nomorTelepon, String kategori)
 {
-    this.id = id;
-    this.nama = nama;
-    this.nomorTelepon = nomorTelepon;
-    this.kategori = kategori;
+this.id = id;
+this.nama = nama;
+this.nomorTelepon = nomorTelepon;
+this.kategori = kategori;
 }
 // Getters dan setters
 public int getId() {
-    return id;
+return id;
 }
 public void setId(int id) {
-    this.id = id;
+this.id = id;
 }
 public String getNama() {
-    return nama;
+return nama;
 }
 public void setNama(String nama) {
-    this.nama = nama;
+this.nama = nama;
 }
 public String getNomorTelepon() {
-    return nomorTelepon;
+return nomorTelepon;
 }
 public void setNomorTelepon(String nomorTelepon) {
-    this.nomorTelepon = nomorTelepon;
+this.nomorTelepon = nomorTelepon;
 }
 public String getKategori() {
-    return kategori;
+return kategori;
 }
 public void setKategori(String kategori) {
-    this.kategori = kategori;
+this.kategori = kategori;
+}
 }
